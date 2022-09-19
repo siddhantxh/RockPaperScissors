be288c20de6b5d98cd60ce6428240a642617eeb4
siddhantxh <113815634+siddhantxh@users.noreply.github.com>
2022-09-19 18:37:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,29 @@ function getComputerChoice() {
   let computerChoice = rpsChoices[Math.floor(Math.random() * 3)]
   return computerChoice
 }
+
+function getResult(playerChoice, computerChoice) {
+    
+  let score;
+
+  if (playerChoice === computerChoice) {
+    score = 0
+
+  
+  } else if (playerChoice === 'Rock' && computerChoice === 'Scissors') {
+    score = 1
+
+  } else if (playerChoice === "Paper" && computerChoice === "Rock") {
+    score = 1
+
+  } else if (playerChoice === "Scissors" && computerChoice === "Paper") {
+    score = 1
+
+  } else {
+    score = -1
+  }
+
+  return score
+}
+
+
