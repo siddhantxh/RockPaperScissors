d30b0885fd7492390e9ed9082abb55f73ac5ea87
siddhantxh <113815634+siddhantxh@users.noreply.github.com>
2022-09-19 18:39:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -56,3 +56,26 @@ function onClickRPS(playerChoice) {
   showResult(score, playerChoice.value, computerChoice)
 }
 
+function playGame() {
+
+  let rpsButtons = document.querySelectorAll('.rpsButton')
+
+  rpsButtons.forEach(rpsButton => {
+    rpsButton.onclick = () => onClickRPS(rpsButton)
+  })
+
+  let endGameButton = document.getElementById('endGameButton')
+  endGameButton.onclick = () => endGame()
+}
+
+function endGame() {
+  let playerScore = document.getElementById('player-score')
+  let hands = document.getElementById('hands')
+  let result = document.getElementById('result')
+  playerScore.innerText = ''
+  hands.innerText = ''
+  result.innerText = ''
+}
+
+playGame()
+
